a571f3f3ec8da24ffda4bb022fce1f00d43414c9
Vedant Anand <vedantanandva28@gmail.com>
2022-05-07 11:14:31 +0530

    Assignment commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [authToken, setAuthToken] = useState('');
+  // const [searchPar, setSearchPar] = useState('');
+  const fetchRes = (e) => {
+    axios.get(`http://3.108.244.88:5000/api/data?search_string=${e.target.value}`, { headers: {"user-access-token": `${authToken}`}})
+      .then(res => {
+        if (res.status === 200) {
+          setSearchResults(res.data);
+        }
+      });
+  }
+  useEffect(() => {
+    axios.get('http://3.108.244.88:5000/api/user-access-token')
+      .then(res => {
+        if (res.status === 200)
+          setAuthToken(res.data.token);
+        else 
+          alert('Failed to authenticate');
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onInput={(e) => fetchRes(e)}></input>
+      <ul>
+        {searchResults.map(stock => {
+          return <li>{stock}</li>
+        })}
+      </ul>
     </div>
   );
 }
